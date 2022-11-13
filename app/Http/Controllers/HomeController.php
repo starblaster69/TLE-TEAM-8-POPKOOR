5130c478ced74ad5226ac812714fd7c2f85e6fb1
starblaster69 <staradiamewmew@gmail.com>
2022-11-13 21:56:23 +0100

    Authentication implemented

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('posts.show');
+//        $this->authenticated();
+    }
+
+    protected function authenticated() {
+        if (Auth::check()) {
+            return redirect()->route('PostController');
+        }
     }
 }
