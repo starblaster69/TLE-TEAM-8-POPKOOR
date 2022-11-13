9e45f50475f7502bf397744e84ecff63a220e5dc
GianniMS <1008999@hr.nl>
2022-11-13 21:56:58 +0100

    Function Crud, without authorization

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,14 @@
                             </div>
                         </div>
 
+                        <div class="row mb-3">
+                            <label for="role" class="col-md-4 col-form-label text-md-end">{{ __('Select Role') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="role" type="number" min="0" max="2" class="form-control" name="role">
+                            </div>
+                        </div>
+
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::resource('posts', PostController::class);
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
